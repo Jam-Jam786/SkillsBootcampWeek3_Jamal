13b38aba33c1188cc96be57bcfabb57f298d934f
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-02 11:29:55 +0000

    RandomIndexInSplicing
    applying a math.random() function in order to make the spliced item appear in the list at any position.

--- SplicePractice.js
+++ SplicePractice.js
@@ -1,10 +1,20 @@
-let myArray = ["monday", "Wednesday", "thursday", "friday"]
-// use splice to insert tuesday at the correct position
+// let myArray = ["monday", "Wednesday", "thursday", "friday"]
+// // use splice to insert tuesday at the correct position
 
-myArray.splice(1, 0, "tuesday")
+// myArray.splice(1, 0, "tuesday")
 
-console.log(myArray);
+// console.log(myArray);
 
-myArray = myArray.concat("saturday", "sunday")
+// myArray = myArray.concat("saturday", "sunday")
 
-console.log(myArray);
+// console.log(myArray);
+
+let stuff = ["apple", "orange", "peers"]
+
+let randomIndex = Math.floor(Math.random() * stuff.length)
+
+console.log(`random index : ${randomIndex}`);
+
+stuff.splice(randomIndex, 0, "banana")
+
+console.log(stuff)
