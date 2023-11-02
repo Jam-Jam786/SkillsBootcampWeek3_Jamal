565e0a7c2be8b52c0813bf136ae850233c331ced
Jam-Jam786 <130471711+Jam-Jam786@users.noreply.github.com>
2023-11-02 11:03:41 +0000

    QuestionCreation

---
+++ 3_QuizzQuestions.js
@@ -0,0 +1,26 @@
+console.log("What is the definition of a function?")
+// a - "self contained" modules of code that accomplish a specific task
+// b - outputs a message to the web console
+// c - A template definition of the methods and variables in a particular kind of object
+
+
+console.log("what does 'OOP' stand for?")
+// a - operationial or peghasis
+// b - object oriented programing
+// c - object oriented python
+
+console.log("definition of an array")
+// a - a type of global object that is used to store data
+// b - a type of array to ordered collection or list
+// c - trick question - doesn't apply to javascript
+
+
+console.log("Make the code print out corectly in the console by makeing it say hello world 5 times. Tip there is 4 mistakes")
+for (let i=1; i < ; i++){
+    console.log("Hello")
+    
+    
+for (let i=0; i < 5; i++){
+   console.log("Hello world") 
+}
+
